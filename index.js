8bd76ab287dd1d6c7ad17fc2aca0310cc8a47b55
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-13 20:20:43 +0600

    simple server setup done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Simple Crud server is running");
+})
+
+app.listen(port, () => {
+    console.log(`simple crud server is running on port: ${port}`);
+})
+
