a3243c2ca4fa1cbf6e849a82cfc00e03f18895fe
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-14 19:29:33 +0600

    single user finding api added

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ async function run() {
             res.send(result);
         })
 
+        app.get("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.findOne(query);
+            res.send(result);
+
+        })
+
         app.post('/users', async (req, res) => {
             console.log('user post activated');
             console.log("data in server", req.body)
